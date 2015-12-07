52743f324f79ff8c6532d3d52d8b43dd95d0a1ff
Stefan Born <bornste@gmail.com>
2015-12-07 13:45:22 +0100

    updated compatibility with ILIAS 5.x

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@ $version = "1.0.1";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.2.0";
-$ilias_max_version = "4.4.999";
+$ilias_max_version = "5.99.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Born";
