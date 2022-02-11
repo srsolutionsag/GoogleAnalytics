57f6c3582d6a938fc4ae60dc6cc65c516081ad2d
Fabian Schmid <fabian@sr.solutions>
2022-02-11 11:41:07 +0100

    Code Style

--- classes/class.ilGoogleAnalyticsUIHookGUI.php
+++ classes/class.ilGoogleAnalyticsUIHookGUI.php
@@ -14,119 +14,121 @@ include_once("./Services/UIComponent/classes/class.ilUIHookPluginGUI.php");
  *
  * @ingroup ServicesUIComponent
  */
-class ilGoogleAnalyticsUIHookGUI extends ilUIHookPluginGUI {
-
-	/**
-	 * Modify HTML output of GUI elements. Modifications modes are:
-	 * - ilUIHookPluginGUI::KEEP (No modification)
-	 * - ilUIHookPluginGUI::REPLACE (Replace default HTML with your HTML)
-	 * - ilUIHookPluginGUI::APPEND (Append your HTML to the default HTML)
-	 * - ilUIHookPluginGUI::PREPEND (Prepend your HTML to the default HTML)
-	 *
-	 * @param string $a_comp component
-	 * @param string $a_part string that identifies the part of the UI that is handled
-	 * @param string $a_par  array of parameters (depend on $a_comp and $a_part)
-	 *
-	 * @return array array with entries "mode" => modification mode, "html" => your html
-	 */
-	public function getHTML($a_comp, $a_part, $a_par = array()) {
-		global $DIC;
-
-		// loading a template and this is NOT an async call?
-		if ($a_part == "template_load" && !$DIC->ctrl()->isAsynch()) {
-			// is main template?
-			if (strtolower($a_par['tpl_id']) == "tpl.main.html") {
-				/**
-				 * @var $plugin_object ilGoogleAnalyticsPlugin
-				 */
-				$plugin_object = $this->plugin_object;
-				// get the account information
-				$account_id = $plugin_object->getAccountId();
-				$anonymize_ip = $plugin_object->getAnonymizeIp();
-				$track_downloads = $plugin_object->getTrackDownloads();
-
-				// only proceed if account id is set!
-				if ($account_id != NULL) {
-
-					$html = $a_par['html'];
-					$index = strripos($html, "</body>", - 7);
-					if ($index !== false) {
-						$tmpl = $plugin_object->getTemplate("tpl.ga_script.html", true, true);
-						$tmpl->setVariable("ACCOUNT_ID", $account_id);
-
-						// anonymize?
-						if ($anonymize_ip) {
-							$tmpl->touchBlock("anonymize_ip");
-						}
-
-						// track downloads?
-						if ($track_downloads) {
-							$tmpl->setCurrentBlock("track_downloads");
-							$tmpl->setVariable("ACCOUNT_ID_DOWNLOAD", $account_id);
-							$tmpl->parseCurrentBlock();
-						}
-
-						// insert code
-						$html = substr($html, 0, $index) . $tmpl->get() . substr($html, $index);
-
+class ilGoogleAnalyticsUIHookGUI extends ilUIHookPluginGUI
+{
+    
+    /**
+     * Modify HTML output of GUI elements. Modifications modes are:
+     * - ilUIHookPluginGUI::KEEP (No modification)
+     * - ilUIHookPluginGUI::REPLACE (Replace default HTML with your HTML)
+     * - ilUIHookPluginGUI::APPEND (Append your HTML to the default HTML)
+     * - ilUIHookPluginGUI::PREPEND (Prepend your HTML to the default HTML)
+     *
+     * @param string $a_comp component
+     * @param string $a_part string that identifies the part of the UI that is handled
+     * @param string $a_par  array of parameters (depend on $a_comp and $a_part)
+     *
+     * @return array array with entries "mode" => modification mode, "html" => your html
+     */
+    public function getHTML($a_comp, $a_part, $a_par = [])
+    {
+        global $DIC;
+        
+        // loading a template and this is NOT an async call?
+        if ($a_part == "template_load" && !$DIC->ctrl()->isAsynch()) {
+            // is main template?
+            if (strtolower($a_par['tpl_id']) == "tpl.main.html") {
+                /**
+                 * @var $plugin_object ilGoogleAnalyticsPlugin
+                 */
+                $plugin_object = $this->plugin_object;
+                // get the account information
+                $account_id = $plugin_object->getAccountId();
+                $anonymize_ip = $plugin_object->getAnonymizeIp();
+                $track_downloads = $plugin_object->getTrackDownloads();
+                
+                // only proceed if account id is set!
+                if ($account_id != null) {
+                    
+                    $html = $a_par['html'];
+                    $index = strripos($html, "</body>", -7);
+                    if ($index !== false) {
+                        $tmpl = $plugin_object->getTemplate("tpl.ga_script.html", true, true);
+                        $tmpl->setVariable("ACCOUNT_ID", $account_id);
+                        
+                        // anonymize?
+                        if ($anonymize_ip) {
+                            $tmpl->touchBlock("anonymize_ip");
+                        }
+                        
+                        // track downloads?
+                        if ($track_downloads) {
+                            $tmpl->setCurrentBlock("track_downloads");
+                            $tmpl->setVariable("ACCOUNT_ID_DOWNLOAD", $account_id);
+                            $tmpl->parseCurrentBlock();
+                        }
+                        
+                        // insert code
+                        $html = substr($html, 0, $index) . $tmpl->get() . substr($html, $index);
+                        
                         // get the tag manager information
                         $use_tag_manager = $plugin_object->getUseTagManager();
                         $container_id = $plugin_object->getContainerId();
-
+                        
                         // only add gtm-snippets if the tag manager should be used and the container id is set!
-                        if($use_tag_manager AND $container_id != NULL) {
-
+                        if ($use_tag_manager and $container_id != null) {
+                            
                             $index_gtm_script = strripos($html, "<head>", -6);
                             if ($index_gtm_script !== false) {
-
+                                
                                 $tmpl_gtm_script = $plugin_object->getTemplate(
                                     "tpl.gtm_script.html",
                                     true,
                                     true
                                 );
                                 $tmpl_gtm_script->setVariable("CONTAINER_ID", $container_id);
-
+                                
                                 // insert google tag manager script code
-                                $html = substr($html, 0, $index_gtm_script+6)
+                                $html = substr($html, 0, $index_gtm_script + 6)
                                     . $tmpl_gtm_script->get()
-                                    . substr($html, $index_gtm_script+6);
+                                    . substr($html, $index_gtm_script + 6);
                             }
-
-                            $opening_body_tag = NULL;
+                            
+                            $opening_body_tag = null;
                             preg_match("<body.*>", $html, $opening_body_tag);
-                            if($opening_body_tag != NULL) {
+                            if ($opening_body_tag != null) {
                                 $length_body_tag = strlen($opening_body_tag[0]);
                                 $index_gtm_noscript = strripos(
                                     $html,
                                     $opening_body_tag[0],
                                     -$length_body_tag
                                 );
-
+                                
                                 if ($index_gtm_noscript !== false) {
-
+                                    
                                     $tmpl_gtm_noscript = $plugin_object->getTemplate(
                                         "tpl.gtm_noscript.html",
                                         true,
                                         true
                                     );
                                     $tmpl_gtm_noscript->setVariable("CONTAINER_ID", $container_id);
-
+                                    
                                     // insert google tag manager no-script code
-                                    $html = substr($html, 0, $index_gtm_noscript+$length_body_tag)
+                                    $html = substr($html, 0, $index_gtm_noscript + $length_body_tag)
                                         . $tmpl_gtm_noscript->get()
-                                        . substr($html, $index_gtm_noscript+$length_body_tag);
+                                        . substr($html, $index_gtm_noscript + $length_body_tag);
                                 }
                             }
                         }
-
-						return array( "mode" => ilUIHookPluginGUI::REPLACE, "html" => $html );
-					}
-				}
-			}
-		}
-
-		return array( "mode" => ilUIHookPluginGUI::KEEP, "html" => "" );
-	}
+                        
+                        return ["mode" => ilUIHookPluginGUI::REPLACE, "html" => $html];
+                    }
+                }
+            }
+        }
+        
+        return ["mode" => ilUIHookPluginGUI::KEEP, "html" => ""];
+    }
 }
 
 ?>
